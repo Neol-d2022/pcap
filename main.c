0ddbaaa0954a20eb4dd94484d99aaa1d12305430
Neol-d2022 <neol_d2022@outlook.com>
2016-06-25 23:00:04 +0800

    Device parameter & error handling

--- main.c
+++ main.c
@@ -6,22 +6,31 @@
 
 #include "ethernet.h"
 
-int main(void) {
+int main(int argc, char **argv) {
 	char errbuf_char[PCAP_ERRBUF_SIZE];
 	struct pcap_pkthdr header_pcap_pkthdr;
+	const char *dev = 0;
 	pcap_t *p_pcap_t;
 	const unsigned char *packet_uchar;
 	int res_int;
 	unsigned int xmlLevel_uint = 0;
 	
-	p_pcap_t = pcap_create("enp0s3", errbuf_char);
+	if(argc == 1) dev = "any";
+	else if(argc == 2) dev = argv[1];
+	else {
+		fprintf(stderr, "Usage: pcap_test [Interface]\n");
+		return -1;
+	}
+	
+	p_pcap_t = pcap_create(dev, errbuf_char);
 	if(!p_pcap_t) {
-		fprintf(stderr, "%s", errbuf_char);
+		fprintf(stderr, "[Error] Cannot create pcap_t.\nError Message: %s\n", errbuf_char);
 		return -1;
 	}
 	
 	if((res_int = pcap_activate(p_pcap_t))) {
-		fprintf(stderr, "%d\n", res_int);
+		fprintf(stderr, "[Error] Cannot activate pcap_t(%d).\n", res_int);
+		return -1;
 	}
 	
 	memset(&header_pcap_pkthdr, 0, sizeof(header_pcap_pkthdr));
